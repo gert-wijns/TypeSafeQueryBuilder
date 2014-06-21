f2c4ef291085b79d58f35e242b5e712096ea29c4
Gert Wijns <gert.wijns@gmail.com>
2014-06-21 23:38:37 +0200

    Added tostring to namedparam

--- src/main/java/be/shad/tsqb/NamedParameter.java
+++ src/main/java/be/shad/tsqb/NamedParameter.java
@@ -31,4 +31,9 @@ public class NamedParameter {
     public Object getValue() {
         return value;
     }
+    
+    @Override
+    public String toString() {
+        return String.format("[:%s, %s]", name, value);
+    }
 }
